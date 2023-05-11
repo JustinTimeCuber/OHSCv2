75fd1535eda57003328af239599007a080693077
JustinTimeCuber <justintimecuber@gmail.com>
2023-05-10 21:25:16 -0500

    Create script to escape the changelog for GitHub

---
+++ reformat-changelog.py
@@ -0,0 +1,15 @@
+f = open("Changelog.txt", "r")
+g = open("Changelog-github.txt", "w")
+for l in f:
+    l = l.replace("\\", "\\\\")
+    l = l.replace("*", "\\*")
+    l = l.replace("-", "\\-")
+    l = l.replace("+", "\\+")
+    s = 0
+    while l.startswith(" "):
+        l = l[1:]
+        s += 1
+    l = ("&nbsp;"*s) + l
+    g.write(l)
+f.close();
+g.close();
